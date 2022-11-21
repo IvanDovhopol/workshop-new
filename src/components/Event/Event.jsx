873030bfde5e266559bafa8fe315d9a04727f29e
Ivan Dovhopol <ivandovhopol@gmail.com>
2022-11-21 04:56:44 +0200

    create new workshop

---
+++ src/components/Event/Event.jsx
@@ -0,0 +1,46 @@
+import {
+  FaMapMarkerAlt,
+  FaUserAlt,
+  FaCalendarAlt,
+  FaClock,
+} from 'react-icons/fa';
+import { iconSize } from 'constans';
+import { formatEventStart, formatEventDuration } from 'utils';
+import PropTypes from 'prop-types';
+import { Card, EventName, Info, Chip } from './Event.styled';
+
+export const Event = ({ name, location, speaker, type, start, end }) => {
+  const formattedStart = formatEventStart(start);
+  const formattedDuration = formatEventDuration(start, end);
+  return (
+    <Card>
+      <EventName>{name}</EventName>
+      <Info>
+        <FaMapMarkerAlt size={iconSize.sm} />
+        {location}
+      </Info>
+      <Info>
+        <FaUserAlt size={iconSize.sm} />
+        {speaker}
+      </Info>
+      <Info>
+        <FaCalendarAlt size={iconSize.sm} />
+        {formattedStart}
+      </Info>
+      <Info>
+        <FaClock size={iconSize.sm} />
+        {formattedDuration}
+      </Info>
+      <Chip eventType={type}>{type}</Chip>
+    </Card>
+  );
+};
+
+Event.propTypes = {
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  speaker: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  start: PropTypes.string.isRequired,
+  end: PropTypes.string.isRequired,
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { ThemeProvider } from '@emotion/react';
 import './index.css';
+import { App } from 'components/App/App';
+import { theme } from './constans/theme';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
